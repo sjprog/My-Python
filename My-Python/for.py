ba224867f4de58d0d0dbec7c00f74d105da43137
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-08-20 18:09:23 -0300

    for

--- My-Python/for.py
+++ My-Python/for.py
@@ -115,5 +115,12 @@ for digit in "0165031806510":
     print(digit, end="")
 
 
-# Faça um programa que mostre uma contagem regresiva na tela para o estouro de fogos, indo de 10 até 0, com uma
-# pausa de 1 segundo entre eles.
+# ==============================Faça um programa que mostre uma contagem regresiva na tela para o estouro de fogos, indo de 10 até 0, com uma
+# pausa de 1 segundo entre eles.==================================
+
+# Minha resposta
+import time
+for i in range(0, 11):
+    time.sleep(1)
+    print(i)
+print('Bommmmmmmmmmmm!!!!')
