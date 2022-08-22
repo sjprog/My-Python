f91c049cb3633d8605b6399da0c44f1ef7fc2f19
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-08-22 19:09:12 -0300

    for

--- My-Python/for.py
+++ My-Python/for.py
@@ -136,10 +136,16 @@ print('ACABOU!')
 # =========================================Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
 
 # Minha resposta numeros impares
-for i in range(0, 50):
+for i in range(0, 51):
      if i % 2 != 0:
         print(i)
 # Minha resposta numeros pares
-for i in range(0, 50):
+for i in range(0, 51):
      if i % 2 == 0:
         print(i)
+
+# Resposta mais produtiva
+for n in range(2, 51, 2):
+    print(n, end=' ')
+print('Acabou')
+
