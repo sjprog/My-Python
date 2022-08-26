38899212f78e1f570aa2810b2905a09cb81fb8cf
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-08-25 22:34:38 -0300

    tech

--- My-Python/Tech.py
+++ My-Python/Tech.py
@@ -1,6 +1,6 @@
 
 
-# =================================================== Numpy =====================================================
+# ==================================================== Numpy =======================================================
 
 # https://didatica.tech/o-pacote-numpy-python-para-machine-learning/
 
@@ -25,7 +25,7 @@ print(a.sum()) #  soma da matriz
 print(a.mean()) # media dos elementos da matriz
 print(a.std()) # desvio padrão da matriz
 
-# ========================================= Arquivos com o Pandas - XLSX e CSV =========================================
+# ========================================== Arquivos com o Pandas - XLSX e CSV ===========================================
 
 # crie um arquivo no exel com linhas e colunas.
 
@@ -48,3 +48,35 @@ pip.main(["install", "openpyxl"])
 
 # =================================ou esse========
 pip install defusedxml
+
+
+# ======================================= Introdução ao Pandas ===========================================================
+
+import pandas as pd
+alunos = {'Nome': ['Sidney', 'Pedro', 'Carla', 'Paula'], # vamos criar uma tebela, criando um dicionario e uma lista
+          'Nota':[4, 5, 9, 5.8],
+          'Aprovado':['Não', 'Sim', 'Sim', 'Não']}
+
+dataframe = pd.DataFrame(alunos) # função do panda que transforma a entrada em dataframe
+print(dataframe) # quando se cria dataframe automaticamente se cria indices
+
+
+
+objeto1 = pd.Series([2, 6, 9, 10, 5]) # criar uma lista series, e vetores unidimensionais
+print(objeto1) # tambem adiciona indices
+
+
+import numpy as np  # cria um array com o numpy
+array1 = np.array([2, 6, 9, 10, 5]) # array dimensional
+array2 = np.array([(2, 6, 9, 10, 5), (6, 5, 4, 8, 6)])  # cria o array bidimensional com dois
+print(array1)
+print(array2)
+
+
+objeto2 = pd.Series(array1)  # criando um array no python so funcional pra uma dimensão
+print(objeto2)
+
+objeto2 = pd.Series(array2)  # duas dimensòes nao funciona
+print(objeto2)
+
+# ==================================================== Comandos úteis do Pandas =======================================================
--- My-Python/teste.py
+++ My-Python/teste.py
@@ -1,8 +1,30 @@
 
 
 
-# dados = pd.read_excel('D:/tech/tech.xlsx')
-# print(dados.head(7))
+
 import pandas as pd
-dados2 = pd.read_csv('D:/tech/athlete_events.csv') # da mesma forma porem com arquivo csv
-print(dados2.head())
+alunos = {'Nome': ['Sidney', 'Pedro', 'Carla', 'Paula'], # vamos criar uma tebela, criando um dicionario e uma lista
+          'Nota':[4, 5, 9, 5.8],
+          'Aprovado':['Não', 'Sim', 'Sim', 'Não']}
+
+dataframe = pd.DataFrame(alunos) # função do panda que transforma a entrada em dataframe
+print(dataframe) # quando se cria dataframe automaticamente se cria indices
+
+
+
+objeto1 = pd.Series([2, 6, 9, 10, 5]) # criar uma lista series, e vetores unidimensionais
+print(objeto1) # tambem adiciona indices
+
+
+import numpy as np  # cria um array com o numpy
+array1 = np.array([2, 6, 9, 10, 5]) # array dimensional
+array2 = np.array([(2, 6, 9, 10, 5), (6, 5, 4, 8, 6)])  # cria o array bidimensional com dois
+print(array1)
+print(array2)
+
+
+objeto2 = pd.Series(array1)  # criando um array no python so funcional pra uma dimensão
+print(objeto2)
+
+objeto2 = pd.Series(array2)  # duas dimensòes nao funciona
+print(objeto2)
