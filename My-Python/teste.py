368bd02a37c9956ba1e93b1612c585d8c3931676
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-08-30 20:32:27 -0300

    Update teste.py

--- My-Python/teste.py
+++ My-Python/teste.py
@@ -1,11 +1,3 @@
-import pandas as pd
 
-dados = pd.read_csv('D:/tech/athlete_events.csv')  # da mesma forma porem com arquivo csv
-
-
-dados.drop('ID', axis = 1, inplace = True) # exclui essas colunas
-dados.drop('City', axis = 1, inplace = True)
-dados.drop('Season', axis = 1, inplace = True)
-print(dados.columns)
 
 
