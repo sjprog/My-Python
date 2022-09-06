785fc28bf69e31dbdca887a10c2a22aca6c6d912
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-09-06 15:09:08 -0300

    tech

--- My-Python/teste.py
+++ My-Python/teste.py
@@ -1,3 +1,10 @@
+
+def warn(*args, **kwargs):  # avisos
+    pass
+import warnings
+warnings.warn = warn
+
+
 import pandas as pd
 prev = pd.read_csv('C:/Users/EXCALIBUR-04/OneDrive/Documentos/GitHub/My-Python/My-Python/exemploscsv/Prev.csv') # carregamento
 # da vase prev
@@ -10,7 +17,7 @@ try:
     amanha = pd.read_csv('C:/Users/EXCALIBUR-04/OneDrive/Documentos/GitHub/My-Python/My-Python/exemploscsv/Futuro.csv')
     print('Fechamento atual:', amanha['Close'][0])
     base = base.append(amanha[:1], sort=True)
-    amanha = amanha.drop(amanha[:1].index, axis = 0)
+    amanha = amanha.drop(amanha[:1].index, axis=0)
     base.to_csv('C:/Users/EXCALIBUR-04/OneDrive/Documentos/GitHub/My-Python/My-Python/exemploscsv/Hoje.csv', index=False)
     amanha.to_csv('C:/Users/EXCALIBUR-04/OneDrive/Documentos/GitHub/My-Python/My-Python/exemploscsv/Futuro.csv', index=False)
 except Exception:
@@ -18,10 +25,10 @@ except Exception:
     pass
 
 base['target'] = base['Close'][1:len(base)].reset_index(drop=True)
-prev = base[-1::].drop('target', axis = 1)
-treino = base.drop(base[-1::].index, axis = 0)
+prev = base[-1::].drop('target', axis=1)
+treino = base.drop(base[-1::].index, axis=0)
 
-treino.loc[treino['target'] > treino['Close'],'target'] = 1
+treino.loc[treino['target'] > treino['Close'], 'target'] = 1
 treino.loc[treino['target'] != 1, 'target'] = 0
 
 y = treino['target']
@@ -43,3 +50,5 @@ if prev['target'][0] == 1:
     print('VAI SUBIR!!!')
 else:
     print('vai cais.')
+
+prev.to_csv('C:/Users/EXCALIBUR-04/OneDrive/Documentos/GitHub/My-Python/My-Python/exemploscsv/Prev.csv', index=False)
