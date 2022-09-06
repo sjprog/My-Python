7cf8ec7051be1c8fd4ccbf3950073fcc8893e1e6
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-09-05 23:25:01 -0300

    tech

--- My-Python/Tech.py
+++ My-Python/Tech.py
@@ -407,3 +407,47 @@ print(plt.show())
 import matplotlib.pyplot as plt
 dados.boxplot(column=['idade','numero','contato']) # dessa forma ele mostra varios boxplot
 print(plt.show())
+
+# =============================== Criando e visualizando gráficos com Matplotlib =======================================
+# anaconda ja tem instalado
+
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  # criari essas duas listas para mostrar como funciona.
+y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+plt.scatter(x,y) # chamando a função e dizer quais variaveis iremos chamar, nessse caso x e y
+print(plt.show()) # chamar o grafico
+
+import numpy as np
+x1 = np.arange(0,1000,1) # arange cria um array nesse caso começa em 0 vai ate 1000 e pula de 1 em 1
+plt.plot(x1, x1 **2) # modifica o comportamento de uma função.
+print(plt.show())
+
+# ==========================================================
+
+import pandas as pd # pd = apelida para facilitar
+dados = pd.read_excel('D:/tech/tech.xlsx')
+
+masculinos = dados.loc[dados['nome']=='paulo'] # dessa forma ele filme apenas os paulos, nesse exemplo.
+print(masculinos.head())
+#   nome  idade cidade  contato   casa  numero
+# 5  paulo     32  paulo       32  paulo      32
+# 6  paulo     33  paulo       33  paulo      33
+# 7  paulo     34  paulo       34  paulo      34
+# 8  paulo     35  paulo       35  paulo      35
+# 9  paulo     36  paulo       36  paulo      36
+
+# ==========================================================
+
+import pandas as pd # pd = apelida para facilitar
+dados = pd.read_excel('D:/tech/tech.xlsx')
+
+import matplotlib.pyplot as plt
+masculinos = dados.loc[dados['nome']=='paulo'] # dessa forma ele filme apenas os paulos, nesse exemplo.
+a = masculinos['nome']
+p = masculinos['idade']
+
+plt.scatter(a,p)  # mostra outro grafico
+print(plt.show())
+
+# ====================================== Como trabalhar com dados faltantes ============================================
--- My-Python/teste.py
+++ My-Python/teste.py
@@ -1,2 +1,27 @@
 
 
+import pandas as pd # pd = apelida para facilitar
+dados = pd.read_excel('D:/tech/tech.xlsx')
+
+
+import matplotlib.pyplot as plt
+# x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  # criari essas duas listas para mostrar como funciona.
+# y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# plt.scatter(x,y) # chamando a função e dizer quais variaveis iremos chamar, nessse caso x e y
+# print(plt.show()) # chamar o grafico
+#
+# import numpy as np
+# x1 = np.arange(0,1000,1) # arange cria um array nesse caso começa em 0 vai ate 1000 e pula de 1 em 1
+# plt.plot(x1, x1 **2) # modifica o comportamento de uma função.
+# print(plt.show())
+import pandas as pd # pd = apelida para facilitar
+dados = pd.read_excel('D:/tech/tech.xlsx')
+
+import matplotlib.pyplot as plt
+masculinos = dados.loc[dados['nome']=='paulo'] # dessa forma ele filme apenas os paulos, nesse exemplo.
+a = masculinos['nome']
+p = masculinos['idade']
+
+plt.scatter(a,p)  # mostra outro grafico
+print(plt.show())
